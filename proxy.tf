d8b0c4f171d639fbd7f6c6fe9ab4f4837ec263dc
TecHunter <TecHunter@users.noreply.github.com>
2020-08-05 13:13:25 +0200

    change order and remove quotes in format

--- proxy.tf
+++ proxy.tf
@@ -14,23 +14,23 @@ resource "aws_api_gateway_resource" "ApiProxyResource" {
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_integration" "ApiProxyIntegration" {
-  rest_api_id              = aws_api_gateway_rest_api.ApiGateway.id
-  resource_id              = aws_api_gateway_resource.ApiProxyResource.id
-    http_method              = aws_api_gateway_method.ApiProxyMethod.http_method
-    type                     = "HTTP_PROXY"
-    integration_http_method  = "ANY"
-    uri                      = format("%s/{proxy}", "${var.base_url}")
-    passthrough_behavior     = "WHEN_NO_MATCH"
-    # request_parameters       = var.aws_api_gateway_integration_request_parameters
-}
-
 resource "aws_api_gateway_method" "ApiProxyMethod" {
   rest_api_id                   = aws_api_gateway_rest_api.ApiGateway.id
   resource_id                   = aws_api_gateway_resource.ApiProxyResource.id
   http_method                   = "ANY"
   authorization                 = "NONE"
   request_parameters            = {"method.request.path.proxy" = true}
+}
+
+resource "aws_api_gateway_integration" "ApiProxyIntegration" {
+  rest_api_id              = aws_api_gateway_rest_api.ApiGateway.id
+  resource_id              = aws_api_gateway_resource.ApiProxyResource.id
+  http_method              = aws_api_gateway_method.ApiProxyMethod.http_method
+  type                     = "HTTP_PROXY"
+  integration_http_method  = "ANY"
+  uri                      = format("%s/{proxy}", var.base_url)
+  passthrough_behavior     = "WHEN_NO_MATCH"
+    # request_parameters       = var.aws_api_gateway_integration_request_parameters
 }
 
 resource "aws_api_gateway_deployment" "ApiDeployment" {
