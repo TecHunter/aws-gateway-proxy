5d2f9bc232b62d131d8589719cf1b3e1a431ac04
TecHunter <TecHunter@users.noreply.github.com>
2020-08-05 13:00:14 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default = "eu-central-1"
+  type = string
+}
+
+variable "profile" {
+  default	= "default"
+  type = string
+}
+
+variable "stage" {
+  default = "prod"
+  type = string
+}
+
+variable "base_url" {
+  default = "https://registry.npmjs.org/"
+  type = string
+}
+
+variable "api_name" {
+  type = string
+}
+
+variable "api_description" {
+  type = string
+}
