19d2e8936e7a4153b2ec85e5202f24655257a9e3
TecHunter <TecHunter@users.noreply.github.com>
2020-08-05 13:02:42 +0200

    fix missing field authorization

--- proxy.tf
+++ proxy.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 data "aws_iam_role" "api_user" {
-  role_name = "api_user"
+  name = "api_user"
 }
 
 resource "aws_api_gateway_rest_api" "ApiGateway" {
@@ -33,6 +33,7 @@ resource "aws_api_gateway_method" "ApiProxyMethod" {
   rest_api_id                   = aws_api_gateway_rest_api.ApiGateway.id
   resource_id                   = aws_api_gateway_resource.ApiProxyResource.id
   http_method                   = "ANY"
+  authorization                 = "NONE"
   request_parameters            = {"method.request.path.proxy" = true}
 }
 
