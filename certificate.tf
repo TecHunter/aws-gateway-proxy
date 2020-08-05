0bdd16b9a0cfb6077f1c630fdfd205119aee7cd4
TecHunter <a.baschenis@gmail.com>
2020-08-05 15:15:10 +0200

    fix missing validation route. try to wait

--- certificate.tf
+++ certificate.tf
@@ -1,4 +1,3 @@
-
 resource "aws_acm_certificate" "subdomain" {
   domain_name = "${var.subdomain}.${var.domain}"
   validation_method = "DNS"
@@ -9,26 +8,31 @@ resource "aws_acm_certificate" "subdomain" {
 }
 
 
-resource "aws_route53_record" "example" {
+resource "aws_route53_record" "validation" {
   for_each = {
-    for dvo in aws_acm_certificate.subdomain.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
+  for dvo in aws_acm_certificate.subdomain.domain_validation_options : dvo.domain_name => {
+    name = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type = dvo.resource_record_type
+  }
   }
 
   allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.selected.zone_id
+  name = each.value.name
+  records = [
+    each.value.record
+  ]
+  ttl = 60
+  type = each.value.type
+  zone_id = data.aws_route53_zone.selected.zone_id
 }
 
 resource "aws_acm_certificate_validation" "validation" {
   certificate_arn = aws_acm_certificate.subdomain.arn
   validation_record_fqdns = [
     aws_acm_certificate.subdomain.domain_name
+  ]
+  depends_on = [
+    aws_route53_record.validation
   ]
 }
