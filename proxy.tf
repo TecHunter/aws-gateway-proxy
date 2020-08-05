4e373db952a5b6629e3f5fb246c7ec4173656d45
TecHunter <a.baschenis@gmail.com>
2020-08-05 13:32:40 +0200

    fix deployment and remove format call

--- proxy.tf
+++ proxy.tf
@@ -11,12 +11,14 @@ resource "aws_api_gateway_rest_api" "ApiGateway" {
 resource "aws_api_gateway_resource" "ApiProxyResource" {
   rest_api_id = aws_api_gateway_rest_api.ApiGateway.id
   parent_id   = aws_api_gateway_rest_api.ApiGateway.root_resource_id
+
   path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "ApiProxyMethod" {
   rest_api_id                   = aws_api_gateway_rest_api.ApiGateway.id
   resource_id                   = aws_api_gateway_resource.ApiProxyResource.id
+
   http_method                   = "ANY"
   authorization                 = "NONE"
   request_parameters            = {"method.request.path.proxy" = true}
@@ -26,14 +28,22 @@ resource "aws_api_gateway_integration" "ApiProxyIntegration" {
   rest_api_id              = aws_api_gateway_rest_api.ApiGateway.id
   resource_id              = aws_api_gateway_resource.ApiProxyResource.id
   http_method              = aws_api_gateway_method.ApiProxyMethod.http_method
+
   type                     = "HTTP_PROXY"
   integration_http_method  = "ANY"
-  uri                      = format("%s/{proxy}", var.base_url)
+  uri                      = "${var.base_url}/{proxy}"
   passthrough_behavior     = "WHEN_NO_MATCH"
-    # request_parameters       = var.aws_api_gateway_integration_request_parameters
+  request_parameters       =    {
+    integration.request.path.proxy = "method.request.path.proxy"
+  }
 }
 
 resource "aws_api_gateway_deployment" "ApiDeployment" {
+  depends_on = [aws_api_gateway_integration.ApiProxyIntegration]
   rest_api_id = aws_api_gateway_rest_api.ApiGateway.id
+
   stage_name = var.stage
+  lifecycle {
+    create_before_destroy = true
+  }
 }
