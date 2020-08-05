61e8f6f7841b3d14cb4717929a923f8f221401df
TecHunter <TecHunter@users.noreply.github.com>
2020-08-05 12:53:18 +0200

    Update proxy.tf

--- proxy.tf
+++ proxy.tf
@@ -37,7 +37,6 @@ resource "aws_api_gateway_method" "ApiProxyMethod" {
 }
 
 resource "aws_api_gateway_deployment" "ApiDeployment" {
-  depends_on = aws_api_gateway_method.ApiProxyMethod
   rest_api_id = aws_api_gateway_rest_api.ApiGateway.id
   stage_name = var.stage
 }
